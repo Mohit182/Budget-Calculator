bebac6f5d17a11ad29398032dbb9c14f33caf707
Mohit Gupta <mohitguptatech18@gmail.com>
2021-11-14 18:21:29 +0530

    seted up project with initials

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
+import Alert from "./Components/Alert";
+import ExpenseForm from "./Components/ExpenseForm";
+import ExpenseList from "./Components/ExpenseList";
 
-function App() {
+const initialExpenses = [
+  { id: 1, charge: "rent", amount: 1600 },
+  { id: 2, charge: "car payment", amount: 400 },
+  { id: 3, charge: "credit card bill", amount: 1200 },
+];
+
+console.log(initialExpenses);
+
+const App = () => {
   return (
-    <div className="App">
-      <h>Hii</h>
-    </div>
+    <>
+      <Alert />
+      <ExpenseList />
+      <ExpenseForm />
+    </>
   );
-}
+};
 
 export default App;
