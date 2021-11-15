6117800e723c3fcabf4ce2aaaf034bfdbe78717c
Mohit Gupta <mohitguptatech18@gmail.com>
2021-11-15 23:36:23 +0530

    saved the expenses in the local storage using localStorage.getItem

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Alert from "./Components/Alert";
 import ExpenseForm from "./Components/ExpenseForm";
 import ExpenseList from "./Components/ExpenseList";
 import { v4 as uuidv4 } from "uuid";
 
-const initialExpenses = [
-  { id: uuidv4(), charge: "rent", amount: 1600 },
-  { id: uuidv4(), charge: "car payment", amount: 400 },
-  { id: uuidv4(), charge: "credit card bill", amount: 1200 },
-];
+// const initialExpenses = [
+//   { id: uuidv4(), charge: "rent", amount: 1600 },
+//   { id: uuidv4(), charge: "car payment", amount: 400 },
+//   { id: uuidv4(), charge: "credit card bill", amount: 1200 },
+// ];
+
+const initialExpenses = localStorage.getItem("expenses")
+  ? JSON.parse(localStorage.getItem("expenses"))
+  : [];
 
 const App = () => {
   const [expenses, setExpenses] = useState(initialExpenses);
@@ -18,6 +22,11 @@ const App = () => {
   const [alert, setAlert] = useState({ show: false });
   const [edit, setEdit] = useState(false);
   const [id, setId] = useState(0);
+
+  useEffect(() => {
+    console.log("we called useeffect");
+    localStorage.setItem("expenses", JSON.stringify(expenses));
+  }, [expenses]);
 
   const chargeHandler = (event) => {
     setCharge(event.target.value);
